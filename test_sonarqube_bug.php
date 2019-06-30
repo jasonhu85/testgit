1c129d2ba299ed2e1b4967edf82173f13fc15160
jasonhu85 <31474872+jasonhu85@users.noreply.github.com>
2019-06-30 21:44:41 +0800

    Create test_sonarqube_bug.php

---
+++ test_sonarqube_bug.php
@@ -0,0 +1,12 @@
+<?php
+//=+ should not be used instead of +=
+$target = -5;
+$num = 3;
+$target =- $num;
+
+//"exit(...)" and "die(...)" statements should not be used
+if ($num == 3) {
+  die(123);
+}
+
+//
